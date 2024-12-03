295a67c8e598b57496f2aeaab67db1c53b71334f
Ryoji-Miyata <mryog0426@gmail.com>
2024-12-04 05:32:48 +0900

    flanker説明文微修正

--- config/jp.js
+++ config/jp.js
@@ -6,7 +6,7 @@ const lang = {
   instructions: {
     gonogo: "<div class='text'><p>数字が表示されたら<b>スペースキー</b>を押してください。</p><p>数字<b>3</b>が表示されたら何も押さないでください。</p></div>",
     stroop: '<div class="text"><p>文字の色に対応するボタンを押してください。</p><p>単語の意味については考えないでください。</p></div>',
-    flanker: '<div class="text"><p>5つの矢印が表示されます。</p><p>真ん中の矢印を見てください。</p><p>左向きの矢印が表示されたら<b>F</b>、それ以外は<b>J</b>を押してください。</p></div>',
+    flanker: '<div class="text"><p>5つの不等号が表示されます。</p><p>真ん中の矢印を見てください。</p><p>＞が表示されたら<b>F</b>、＜が表示されたら<b>J</b>を押してください。</p></div>',
     letter_memory: (askTarget) => {
       return `<div class="text"><p>この実験では、画面の中央に1つずつ文字が表示されます。</p><p>最後の<b>${askTarget}</b>つの文字を覚えてください。</p><p>最後の文字が消えた後、覚えている文字を入力してください。</p></div>`
     },
